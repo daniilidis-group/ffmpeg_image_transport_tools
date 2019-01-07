b8565f74558551c58530d79be569de49c0379318
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2019-01-07 16:35:37 -0500

    parallel decoding for greater speed

--- include/ffmpeg_image_transport_tools/decode_bag.h
+++ include/ffmpeg_image_transport_tools/decode_bag.h
@@ -13,10 +13,14 @@
 
 #include <opencv2/core/core.hpp>
 
+#include <boost/thread.hpp>
+
 #include <fstream>
 #include <vector>
 #include <unordered_map>
 #include <memory>
+#include <mutex>
+#include <condition_variable>
 
 namespace ffmpeg_image_transport_tools {
   class DecodeBag {
@@ -26,16 +30,49 @@ namespace ffmpeg_image_transport_tools {
     typedef flex_sync::Sync<sensor_msgs::Image> ImageSync;
     typedef std::shared_ptr<ImageSync> ImageSyncPtr;
   public:
+    class CopyThread {
+    public:
+      CopyThread(const cv::Mat &dest);
+      ~CopyThread();
+      inline void startCopy(const ImageConstPtr &msg) {
+        std::unique_lock<std::mutex> lock(mutex_);
+        msgToCopy_ = msg;
+        cv_.notify_all();
+      }
+      inline void waitForCopyComplete() {
+        std::unique_lock<std::mutex> lock(mutex_);
+        while (msgToCopy_ && !threadStopSignaled_) {
+          cv_.wait(lock);
+        }
+        msgToCopy_.reset();
+      }
+      void run();
+    private:
+      std::mutex              mutex_;
+      std::condition_variable cv_;
+      bool                    threadStopSignaled_{false};
+      ImageConstPtr           msgToCopy_;
+      cv::Mat                 dest_;
+      boost::shared_ptr<boost::thread> thread_;
+    };
     class Session {
     public:
       Session(const std::string &topic, const ImageSyncPtr &sync);
+      ~Session();
       void callback(const ImageConstPtr &img);
       void processMessage(const FFMPEGPacketConstPtr &msg);
     private:
+      void decodeThread();
       // --------- variables
-      std::string   topic_;
-      ImageSyncPtr  sync_;
+      std::string             topic_;
+      ImageSyncPtr            sync_;
+      unsigned int            packetNum_{0};
+      std::mutex              mutex_;
+      std::condition_variable cv_;
       ffmpeg_image_transport::FFMPEGDecoder decoder_;
+      FFMPEGPacketConstPtr    msgToDecode_;
+      boost::shared_ptr<boost::thread> thread_;
+      bool                    threadStopSignaled_{false};
     };
     
     DecodeBag(const ros::NodeHandle& pnh);
@@ -45,20 +82,27 @@ namespace ffmpeg_image_transport_tools {
   private:
     void processBag(const std::string &fname);
     void packetReady(const FFMPEGPacketConstPtr &pkt);
+    void startCopyThreads(int w, int h, int size);
     // ------------------------ variables --------
     ros::NodeHandle   nh_;
     typedef std::shared_ptr<Session> SessionPtr;
     typedef std::unordered_map<std::string, SessionPtr> SessionMap;
+    std::vector<std::shared_ptr<CopyThread>> copyThreads_;
     ImageSyncPtr  sync_;
     ffmpeg_image_transport::FFMPEGEncoder encoder_;
     SessionMap    sessions_;
     int           numRows_;
     int           numCols_;
+    cv::Mat       fullImg_;
     unsigned int  frameNum_{0};
     int           maxNumFrames_;
     std::vector<int>  cameraToLocation_;
     std::vector<std::string> imageTopics_;
     std::ofstream rawStream_;
+    bool          writeFrames_{false};
+    bool          writeVideo_{false};
+    int           keepRatio_{100};
+    std::string   baseName_;
   };
 
 }
--- src/decode_bag.cpp
+++ src/decode_bag.cpp
@@ -17,6 +17,18 @@ namespace ffmpeg_image_transport_tools {
   DecodeBag::Session::Session(const std::string &topic,
                               const ImageSyncPtr &sync) :
     topic_(topic), sync_(sync) {
+    thread_ = boost::make_shared<boost::thread>(
+      &DecodeBag::Session::decodeThread, this);
+  }
+  DecodeBag::Session::~Session() {
+    if (thread_) {
+      {
+        std::unique_lock<std::mutex> lock(mutex_);
+        threadStopSignaled_ = true;
+        cv_.notify_all();
+      }
+      thread_->join();
+    }
   }
 
   DecodeBag::DecodeBag(const ros::NodeHandle& pnh) :  nh_(pnh) {
@@ -26,11 +38,56 @@ namespace ffmpeg_image_transport_tools {
   }
 
   void DecodeBag::packetReady(const FFMPEGPacketConstPtr &pkt) {
-    ROS_INFO("writing frame: %5u", frameNum_);
+    //ROS_INFO("writing frame: %5u", frameNum_);
     rawStream_.write((const char *)(&pkt->data[0]), pkt->data.size());
-    frameNum_++;
   }
 
+  DecodeBag::CopyThread::CopyThread(const cv::Mat &dest) : dest_(dest) {
+    thread_ = boost::make_shared<boost::thread>(
+      &DecodeBag::CopyThread::run, this);
+  }
+
+  DecodeBag::CopyThread::~CopyThread() {
+    if (thread_) {
+      {
+        std::unique_lock<std::mutex> lock(mutex_);
+        threadStopSignaled_ = true;
+        cv_.notify_all();
+      }
+      thread_->join();
+    }
+  }
+
+  void DecodeBag::CopyThread::run() {
+    std::unique_lock<std::mutex> lock(mutex_);
+    while (!threadStopSignaled_ && ros::ok()) {
+      while (!msgToCopy_ && !threadStopSignaled_) {
+        cv_.wait(lock);
+      }
+      if (msgToCopy_) {
+        cv_bridge::toCvShare(msgToCopy_,
+               sensor_msgs::image_encodings::BGR8)->image.copyTo(dest_);
+      }
+      msgToCopy_.reset();
+      cv_.notify_all();
+    }
+  }
+
+  void DecodeBag::startCopyThreads(int w, int h, int size) {
+    for (int i = 0; i < size; i++) {
+      const int col = cameraToLocation_[i] % numCols_;
+      const int row = cameraToLocation_[i] / numCols_;
+      cv::Mat dst = fullImg_(cv::Rect(col * w, row * h, w, h));
+      copyThreads_.emplace_back(new CopyThread(dst));
+    }
+  }
+
+  static std::string
+  make_file_name(const std::string &prefix, const unsigned int counter) {
+    std::stringstream ss;
+    ss << prefix << "_" << std::setfill('0') << std::setw(6) << counter << ".jpg";
+    return (ss.str());
+  }
 
   void DecodeBag::syncCallback(const std::vector<ImageConstPtr> &msgs) {
     if (msgs.size() == 0) {
@@ -43,41 +100,52 @@ namespace ffmpeg_image_transport_tools {
       ROS_WARN("image0 not full size!");
       return;
     }
-    // arrange individual image into full image
-    cv::Mat fullImg(h * numRows_, w * numCols_,  CV_8UC3);
-    for (const auto i: irange(0ul, msgs.size())) {
-      const auto &msg = msgs[i];
-      const int col = cameraToLocation_[i] % numCols_;
-      const int row = cameraToLocation_[i] / numCols_;
-      cv::Mat dest = fullImg(cv::Rect(col * w, row * h, w, h));
-      cv_bridge::toCvShare(msg,
-                    sensor_msgs::image_encodings::BGR8)->image.copyTo(dest);
+    // arrange individual images into full image
+    if (fullImg_.rows == 0) {
+      fullImg_ = cv::Mat(h * numRows_, w * numCols_,  CV_8UC3);
+      startCopyThreads(w, h, (int)msgs.size());
     }
-    // convert full image to ros message
-    ImageConstPtr imgPtr = cv_bridge::CvImage(msg0->header,
-                                              "bgr8", fullImg).toImageMsg();
-    // encode full image
-    if (!encoder_.isInitialized()) {
-      encoder_.initialize(*imgPtr, boost::bind(&DecodeBag::packetReady,
-                                               this, _1));
+    for (int i = 0; i < (int)msgs.size(); i++) {
+      copyThreads_[i]->startCopy(msgs[i]);
+    }
+    for (int i = 0; i < (int)msgs.size(); i++) {
+      copyThreads_[i]->waitForCopyComplete();
+    }
+    if (writeFrames_ && (frameNum_ % keepRatio_ == 0)) {
+      cv::imwrite(make_file_name(baseName_, frameNum_ / keepRatio_), fullImg_);
+    }
+    frameNum_++;
+
+    if (writeVideo_) {
+      // encode full image
       if (!encoder_.isInitialized()) {
-        ROS_ERROR_STREAM("cannot initialize encoder!");
-        return;
+        encoder_.initialize(fullImg_.cols, fullImg_.rows,
+                            boost::bind(&DecodeBag::packetReady, this, _1));
+        if (!encoder_.isInitialized()) {
+          ROS_ERROR_STREAM("cannot initialize encoder!");
+          return;
+        }
       }
+      // this will trigger a callback to packetReady() when the encoder
+      // has produced the first packet
+      encoder_.encodeImage(fullImg_, msg0->header, ros::WallTime::now());
     }
-    // this will trigger a callback to packetReady() when the encoder
-    // has produced the first packet
-    encoder_.encodeImage(*imgPtr);
   }
 
   bool DecodeBag::initialize() {
-    encoder_.setCodec("hevc_nvenc");
-    encoder_.setProfile("main");
-    encoder_.setPreset("slow");
-    encoder_.setQMax(0);
-    encoder_.setBitRate(10*1024*1024);
-    encoder_.setGOPSize(20);
-    encoder_.setMeasurePerformance(true);
+    nh_.param<std::string>("base_file_name", baseName_, "frame");
+    nh_.param<int>("keep_ratio",   keepRatio_, 100);
+    nh_.param<bool>("write_video",   writeVideo_, true);
+    nh_.param<bool>("write_frames",  writeFrames_, false);
+    if (writeVideo_) {
+      encoder_.setCodec("hevc_nvenc");
+      encoder_.setProfile("main");
+      encoder_.setPreset("slow");
+      encoder_.setQMax(0);
+      encoder_.setBitRate(10*1024*1024);
+      encoder_.setGOPSize(20);
+      encoder_.setMeasurePerformance(false);
+    }
 
     if (!nh_.getParam("image_topics", imageTopics_)) {
       ROS_ERROR("no image topics found!");
@@ -105,15 +173,16 @@ namespace ffmpeg_image_transport_tools {
 
     std::string bagFile;
     nh_.param<std::string>("bag_file",  bagFile,  "");
-    ROS_INFO_STREAM("decoding bag: " << bagFile << " -> " << bagFile);
-    rawStream_.open("raw.h265", std::ios::out | std::ios::binary);
+    std::string outFile;
+    nh_.param<std::string>("output_file",  outFile,  "raw.h265");
+    ROS_INFO_STREAM("decoding bag: " << bagFile << " to " << outFile);
+    rawStream_.open(outFile, std::ios::out | std::ios::binary);
     if (!bagFile.empty()) {
       processBag(bagFile);
     } else {
-      ROS_ERROR_STREAM("must specfiy bag_file!");
+      ROS_ERROR_STREAM("must specify bag_file!");
     }
     rawStream_.close();
-
     ros::shutdown();
     return (true);
   }
@@ -124,6 +193,29 @@ namespace ffmpeg_image_transport_tools {
     sync_->process(topic_, img);
   }
 
+  void DecodeBag::Session::decodeThread() {
+    std::unique_lock<std::mutex> lock(mutex_);
+    while (!threadStopSignaled_ && ros::ok()) {
+      while (!msgToDecode_ && !threadStopSignaled_) {
+        cv_.wait(lock);
+      }
+      if (msgToDecode_) {
+        // this should produce a callback with the decoded image
+        decoder_.decodePacket(msgToDecode_);
+#ifdef MEASURE_DECODER_PERFORMANCE        
+        packetNum_++;
+        if (packetNum_ > 50) {
+          packetNum_ = 0;
+          decoder_.printTimers(topic_);
+          decoder_.resetTimers();
+        }
+#endif        
+        msgToDecode_.reset();
+        cv_.notify_all();
+      }
+    }
+  }
+
   void DecodeBag::Session::processMessage(const FFMPEGPacketConstPtr &msg) {
     if (!decoder_.isInitialized()) {
       decoder_.initialize(msg, boost::bind(&DecodeBag::Session::callback,
@@ -132,7 +224,14 @@ namespace ffmpeg_image_transport_tools {
         return;
       }
     }
-    decoder_.decodePacket(msg);
+    
+    std::unique_lock<std::mutex> lock(mutex_);
+    // wait for previous message to clear
+    while (msgToDecode_ && ros::ok()) {
+      cv_.wait(lock);
+    }
+    msgToDecode_ = msg;
+    cv_.notify_all();
   }
 
   void DecodeBag::processBag(const std::string &fname) {
@@ -161,6 +260,8 @@ namespace ffmpeg_image_transport_tools {
     ROS_INFO_STREAM("using topics: " << all_topics_string);
     ROS_INFO_STREAM("playing from file: " << fname);
     rosbag::View view(bag, rosbag::TopicQuery(topics));
+    auto t0 = ros::WallTime::now();
+    int cnt(0), perfInterval(500);
     for (const rosbag::MessageInstance &m: view) {
       FFMPEGPacketConstPtr msg = m.instantiate<FFMPEGPacket>();
       if (msg) {
@@ -170,8 +271,16 @@ namespace ffmpeg_image_transport_tools {
       if (!ros::ok() || (int)frameNum_ > maxNumFrames_) {
         break;
       }
+      if (cnt++ > perfInterval) {
+        const auto t1 = ros::WallTime::now();
+        ROS_INFO_STREAM("wrote frames: " << frameNum_ << " fps: " <<
+                        perfInterval / (topics.size() * (t1-t0).toSec()));
+        cnt = 0;
+        t0 = t1;
+      }
     }
     bag.close();
+    sessions_.clear(); // should collect threads
   }
   
 }  // namespace
